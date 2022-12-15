dbe0d50fa939947adb6be66971f3829655b8ace6
Ewelina Kociolek <ewelina.kociolek@wp.pl>
2022-12-15 21:45:43 +0100

    add API finish app

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ import './sass/main.scss'
 
 let cityForm = document.querySelector('.weather__form')
 let cityInput = document.querySelector('.weather__city')
+let APIURL = 'http://api.weatherapi.com/v1/current.json?key=f68f28912c724928bb8172103221512&aqi=yes&q='
+let apiView = document.querySelector('.weather__data')
+let loader = document.querySelector('.weather__loader')
+
 
 cityForm.addEventListener('submit', (event) => {
     let city = cityInput.value
@@ -9,6 +13,41 @@ cityForm.addEventListener('submit', (event) => {
         cityInput.classList.add('weather__city--error')
     } else {
         cityInput.classList.remove('weather__city--error')
+        let APIURLWITHCITY = APIURL + city
+        console.log(APIURLWITHCITY)
+        showLoader()
+        fetch(APIURLWITHCITY)
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json()
+                } else {
+                    return showError()
+                }
+            })
+            .then((dataFromAPI) => {
+                hideLoader();
+                console.log(dataFromAPI.current.temp_c)
+                let view = ''
+                view += `<div class="weather__location">
+                ${dataFromAPI.location.name} - ${dataFromAPI.location.country}
+                </div>`
+                view += `<div class="weather__info">`
+                // icon
+                view += `<div class="weather__icon"> <img src="${dataFromAPI.current.condition.icon}" alt="${dataFromAPI.current.condition.text}">
+                </div>`
+                view += `<div class="weather__temp">
+                            <span class="weather__num">${dataFromAPI.current.temp_c}</span>
+                            <span class="weather__unit">&deg;C</span>
+                        </div>`
+                view += `<div class="weather__desc">
+                    <p class="weather__text">The amount of rainfall: ${dataFromAPI.current.precip_mm} mm</p>
+                    <p class="weather__text">Humidity:${dataFromAPI.current.humidity}%</p>
+                    <p class="weather__text">Wind spreed:${dataFromAPI.current.wind_kph}kph</p>
+                </div>`
+                view += `</div>`
+
+                apiView.innerHTML = view
+            })
     }
 
     event.preventDefault()
@@ -23,3 +62,15 @@ cityInput.addEventListener('keyup', () => {
         cityInput.classList.remove('weather__city--error')
     }
 })
+
+let showError = () => {
+    apiView.innerHTML = `<div class="weather__error">City nod found or we have problem with API</div>`
+}
+
+let showLoader = () => {
+    loader.style.display = 'block'
+}
+
+const hideLoader = () => {
+    loader.style.display = 'none';
+}
